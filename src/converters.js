71de31764218430c53a2b9c6216c6ba267b07f0f
waihon <waihon@flux.io>
2016-05-19 17:01:22 -0700

    Changes to use dxf-parse library instead

--- src/converters.js
+++ src/converters.js
@@ -1,49 +1,49 @@
 'use strict';
 function convertObj(inObj) {
-    return new Promise(function(resolve, reject) {
-        var result = {
-            "vertices": [],
-            "faces": [],
-            "primitive": "mesh"
-        };
-        var comp;
-        var lines = inObj.split('\n');
-        for (var i=0;i<lines.length;i++) {
-            var line = lines[i].trim();
-            if ( line[0] === 'v' && line[1] === ' ') { // look for v not vt
-                comp = line.trim().split(/ +/); // split by any number of spaces
-                result.vertices.push([
-                    parseFloat(comp[1]),
-                    parseFloat(comp[3]),
-                    parseFloat(comp[2])
-                ]);
-            } else if (line[0] === 'f') {
-                if (line[line.length-1] === '\\') {
-                    var fullLine = line.substr(0,line.length-1).trim();
-                    while(line[line.length-1] === '\\') {
-                        line = lines[i++].trim();
-                        fullLine += ' '+line.substring(1,line.length-1).trim();
-                    }
-                    line = fullLine;
-                    continue;
-                }
-                comp = line.trim().split(/ +/); // split by any number of spaces
-                var face = [];
-                for (var j=1;j<comp.length;j++) {
-                    var faceIndex = parseInt(comp[j].split('/')[0]);
-                    face.push(faceIndex-1);
-                }
-                result.faces.push(face);
-            }
-            if (result.faces.length > 400111) {
-                // TODO This will crash flux renderer right now
-                console.warn("Sorry, I hope to some day render more than 500K polygons.");
-                break;
-            }
+  return new Promise(function(resolve, reject) {
+    var result = {
+      "vertices": [],
+      "faces": [],
+      "primitive": "mesh"
+    };
+    var comp;
+    var lines = inObj.split('\n');
+    for (var i=0;i<lines.length;i++) {
+      var line = lines[i].trim();
+      if ( line[0] === 'v' && line[1] === ' ') { // look for v not vt
+        comp = line.trim().split(/ +/); // split by any number of spaces
+        result.vertices.push([
+          parseFloat(comp[1]),
+          parseFloat(comp[3]),
+          parseFloat(comp[2])
+        ]);
+      } else if (line[0] === 'f') {
+        if (line[line.length-1] === '\\') {
+          var fullLine = line.substr(0,line.length-1).trim();
+          while(line[line.length-1] === '\\') {
+            line = lines[i++].trim();
+            fullLine += ' '+line.substring(1,line.length-1).trim();
+          }
+          line = fullLine;
+          continue;
         }
-        console.log('Loaded',result.faces.length,'faces and',result.vertices.length,'vertices.');
-        resolve(result);
-    });
+        comp = line.trim().split(/ +/); // split by any number of spaces
+        var face = [];
+        for (var j=1;j<comp.length;j++) {
+          var faceIndex = parseInt(comp[j].split('/')[0]);
+          face.push(faceIndex-1);
+        }
+        result.faces.push(face);
+      }
+      if (result.faces.length > 400111) {
+        // TODO This will crash flux renderer right now
+        console.warn("Sorry, I hope to some day render more than 500K polygons.");
+        break;
+      }
+    }
+    console.log('Loaded',result.faces.length,'faces and',result.vertices.length,'vertices.');
+    resolve(result);
+  });
 }
 
 function convertStl(inStl) {
@@ -79,40 +79,51 @@ function convertStl(inStl) {
 
 function convertDxf(inDxf) {
   return new Promise(function(resolve, reject) {
+    var parsedDxf;
     var result = [];
-    var currentEntityIndex = -1;
+    var parser = new DxfParser();
 
-    var lines = inDxf.split('\n');
-    var processingEntityType = 'NONE';
-    for (var i=0; i<lines.length; i++) {
-      var line = escape(lines[i]);
+    // Synchronously parse the Dxf file using DxfParser to return a parsed object.
+    try {
+      parsedDxf = parser.parseSync(inDxf);
+    } catch(err) {
+      return console.error(err.stack);
+    }
 
-      if (line === 'LINE%0D') {
-        currentEntityIndex += 1;
-        var lineTemplate = {
-          "start": [],
-          "end": [],
-          "primitive": "line"
-        };
-        result[currentEntityIndex] = lineTemplate;
-        processingEntityType = 'LINE';
-      }
+    // Convert to Flux format.
+    for (var i=0; i<parsedDxf.entities.length; i++) {
+      var entity = parsedDxf.entities[i];
 
-      if (processingEntityType === 'LINE') {
-        if (line === "%2010%0D") {
-          result[currentEntityIndex]["start"][0] = parseFloat(lines[i+1]);
-        } else if (line === "%2020%0D") {
-          result[currentEntityIndex]["start"][1] = parseFloat(lines[i+1]);
-        } else if (line === "%2030%0D") {
-          result[currentEntityIndex]["start"][2] = parseFloat(lines[i+1]);
-        } else if (line === "%2011%0D") {
-          result[currentEntityIndex]["end"][0] = parseFloat(lines[i+1]);
-        } else if (line === "%2021%0D") {
-          result[currentEntityIndex]["end"][1] = parseFloat(lines[i+1]);
-        } else if (line === "%2031%0D") {
-          result[currentEntityIndex]["end"][2] = parseFloat(lines[i+1]);
-          processingEntityType = 'NONE';
-        }
+      if (entity.type === 'LINE') {
+        result.push({
+          primitive: 'line',
+          start: [entity.vertices[0].x, entity.vertices[0].y, entity.vertices[0].z],
+          end: [entity.vertices[1].x, entity.vertices[1].y, entity.vertices[1].z],
+          attributes: {
+            materialProperties: {
+              color: binToRGB(entity.color)
+            }
+          },
+        });
+      } else if (entity.type === "POLYLINE") {
+        result.push({
+          primitive: 'polyline',
+          points: entity.vertices.map(function(vert) {
+            return [vert.x, vert.y, vert.z];
+          }),
+          attributes: {
+            materialProperties: {
+              color: binToRGB(entity.color)
+            }
+          },
+        });
+      } else if (entity.type === "POINT") {
+        result.push({
+          primitive: 'point',
+          point: [entity.position.x, entity.position.y, entity.position.z],
+        });
+      } else {
+        console.info('cant parse entity of type ', entity.type);
       }
     }
 
@@ -134,4 +145,13 @@ function convertCsv(inCsv) {
       }
     );
   });
+}
+
+// Convert bin to RGB values.
+function binToRGB(bin) {
+    var pbin = parseInt(bin,2);
+    var r = pbin >> 16;
+    var g = pbin >> 8 & 0xFF;
+    var b = pbin & 0xFF;
+    return [r,g,b];
 }
