f1f06234a851e9144011d2657bea94f2d539c34f
david733 <david733@alum.mit.edu>
2016-10-06 21:05:47 -0700

    Getting it to run locally
    Getting it to run locally

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ var app = express();
 app.set('port', process.env.PORT||8001);
 
 app.get('*',function(req,res,next){
-  if(req.headers['x-forwarded-proto']!='https')
-    res.redirect('https://flux-3d-model-uploader.herokuapp.com'+req.url)
-  else
+  // if(req.headers['x-forwarded-proto']!='https')
+  //   res.redirect('https://flux-3d-model-uploader.herokuapp.com'+req.url)
+  // else
     next() /* Continue to other routes if we're not redirecting */
 });
 
